fe2b337b1def5d3013d199104ba0c8389ed3cf8d
Antonio Morga <anotniomc9719@gmail.com>
2022-06-21 15:16:19 +0200

    Added setLoading action creator

--- src/redux/action-creators.ts
+++ src/redux/action-creators.ts
@@ -2,3 +2,4 @@ import { createAction } from "@reduxjs/toolkit";
 import { Book } from "../models/book";
 
 export const loadBooks = createAction<Book[]>("@books/load");
+export const setLoading = createAction("@books/set-loading");
