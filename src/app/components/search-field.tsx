facfe7fadd4e098e5348938b9241db59abf00797
Antonio Morga <anotniomc9719@gmail.com>
2022-06-21 18:00:51 +0200

    Updated SearchField to use state-setting function provided by QuickSearch component

--- src/app/components/search-field.tsx
+++ src/app/components/search-field.tsx
@@ -1,11 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDebounce } from "../../hooks/use-debounce";
 
-export function SearchField() {
+export function SearchField({
+  updateInputValue,
+}: {
+  updateInputValue: (text: string) => void;
+}) {
   const [inputValue, setInputValue] = useState("");
+  const delay = 300;
+  const debouncedSearchText = useDebounce(inputValue, delay);
 
   const handleChange = (text: string) => {
     setInputValue(text);
   };
+
+  useEffect(() => {
+    updateInputValue(debouncedSearchText);
+  }, [debouncedSearchText, updateInputValue]);
 
   return (
     <input
