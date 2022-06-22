25a244d4c979b4514fc5abfaa10fa4777bfb5d8e
Antonio Morga <anotniomc9719@gmail.com>
2022-06-22 13:20:56 +0200

    Moved search field component to its own folder and added test file

---
+++ src/app/components/search-field/search-field.spec.tsx
@@ -0,0 +1,3 @@
+import { SearchField } from "./search-field";
+
+describe("Given the component SearchField", () => {});
--- src/app/components/search-field/search-field.tsx
+++ src/app/components/search-field/search-field.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useDebounce } from "../../hooks/use-debounce";
+import { useDebounce } from "../../../hooks/use-debounce";
 
 export function SearchField({
   updateInputValue,
